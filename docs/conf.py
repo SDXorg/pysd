4b8bba5b68d8cc96459f04b9c98cf2b11255b04f
James Houghton <James.p.houghton@gmail.com>
2015-06-18 15:49:21 -0400

    Update Doc
    Fix imports (using mock) for readthedocs build.

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,14 @@ import sys
 import os
 import shlex
 
+
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.stats',
+                'scipy.integrate', 'pandas']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
