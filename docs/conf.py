963f7422a006236cc11e91ce46a6a44eaabb7b0c
James Houghton <James.p.houghton@gmail.com>
2015-06-26 15:27:30 -0400

    Update Docs

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
