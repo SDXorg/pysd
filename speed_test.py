10278e10b7c599ccc584cc8754634a052a53622d
James Houghton <James.p.houghton@gmail.com>
2016-01-11 14:13:04 -0500

    speedtests

--- speed_test.py
+++ speed_test.py
@@ -19,7 +19,7 @@ print 'Import:', times['import']
 
 ####################
 
-times['load teacup'] = timeit.timeit("pysd.read_vensim('tests/vensim/Teacup.mdl')",
+times['load teacup'] = timeit.timeit("pysd.read_vensim('tests/old_tests/vensim/Teacup.mdl')",
                                      setup='import pysd', number=100)/100
 
 print 'Load Teacup:', times['load teacup']
@@ -27,7 +27,7 @@ print 'Load Teacup:', times['load teacup']
 ##################
 
 times['run teacup baseline'] = timeit.timeit("model.run()",
-                                    setup="import pysd; model = pysd.read_vensim('tests/vensim/Teacup.mdl')",
+                                    setup="import pysd; model = pysd.read_vensim('tests/old_tests/vensim/Teacup.mdl')",
                                     number=1000)/1000
 
 print 'Run Teacup Baseline:', times['run teacup baseline']
@@ -36,7 +36,7 @@ print 'Run Teacup Baseline:', times['run teacup baseline']
 ##############
 
 times['run teacup modify params'] = timeit.timeit("model.run(params={'room_temperature':20})",
-                                    setup="import pysd; model = pysd.read_vensim('tests/vensim/Teacup.mdl')",
+                                    setup="import pysd; model = pysd.read_vensim('tests/old_tests/vensim/Teacup.mdl')",
                                     number=1000)/1000
 
 print 'Run Teacup Mofifying Params:', times['run teacup modify params']
@@ -45,11 +45,21 @@ print 'Run Teacup Mofifying Params:', times['run teacup modify params']
 ###########
 
 times['run teacup return extra columns'] = timeit.timeit("model.run(return_columns=['teacup_temperature','heat_loss_to_room'])",
-                                    setup="import pysd; model = pysd.read_vensim('tests/vensim/Teacup.mdl')",
+                                    setup="import pysd; model = pysd.read_vensim('tests/old_tests/vensim/Teacup.mdl')",
                                     number=100)/100
 
 print 'Run Teacup Returning Extra Columns:', times['run teacup return extra columns']
 
+###############
 
+times['monte carlo'] = \
+    timeit.timeit("[model.run(initial_condition=(0, {'teacup_temperature':50*np.random.rand()})) for i in range(1000)]",
+                  setup="import numpy as np; import pysd; model = pysd.read_vensim('tests/old_tests/vensim/Teacup.mdl')",
+                  number=10)/10
+
+print 'Monte Carlo:', times['monte carlo']
+
+
+#---------------------------------
 with open('tests/speedtest_results.json', 'a') as outfile:
     outfile.write(str(times)+'\n')
