c633d5f7bbf7ce479f97c0be48420adf2dbe06b9
MiguelEq <equihuam@gmail.com>
2017-09-07 14:20:03 -0500

    Testing with SIR_HK.stmx partially successful

--- translator_xmile/xmile2py.py
+++ translator_xmile/xmile2py.py
@@ -9,24 +9,55 @@ from parsimonious.grammar import Grammar
 from parsimonious.nodes import NodeVisitor
 from bs4 import BeautifulSoup as bs
 import sys
+import os
 
 
 def main():
     if len(sys.argv) < 3:
         print("Usage: xmile2py <input_stella.stmx> <outname>\n")
-        print("\t -- input_stella.stmx is a xml variant text file " +
-              " use in STELLA models from version 10 and up\n")
-        print("\t -- outname is the name of R project where the main R" +
-              " script (outname.R) as well as data files and other" +
-              " functions are stored\n")
+        print("\t -- input_stella.stmx. This file type is a xml variant" +
+              " text file use in STELLA models from version 10 and up\n")
+        print("\t -- outname is the name of an R project directory where" +
+              " the main R script (outname.R), data files and other" +
+              " functions will be stored\n")
         sys.exit()
 
-    fname1 = sys.argv[1]
+    # exctract data from command line input
+    model_file = sys.argv[1]
     outputName = sys.argv[2]
-    outPath = outputName
-    outName_split = re.split('(\\\|/)', outputName)
-    if len(outName_split) > 1:
-        outputName = outName_split[-1]
+    outPath = os.path.abspath(outputName)
+    outName = os.path.basename(outputName)
+
+    # Prepare directory to store output
+    if (not os.path.isdir(outPath)):
+        os.mkdir(outPath)
+
+# os.chdir("C:/Users/Miguel/Documents/1 Nube/Dropbox/Curso posgrado/2015/Día 2/Influenza/El modelo general")
+# model_file = "SIR_HK (stella 10).stmx"
+# os.chdir("C:/Users/Miguel/Documents/0 Versiones/2 Proyectos/pysd/translator_xmile")
+# model_file = "SIR_HK.stmx"
+
+    # Read model file
+    with open(model_file) as fmodel:
+        model_soup = bs(fmodel, "lxml")
+
+    # Extract relevant information to build model
+    model_name = model_soup.header.find("name").text
+    print("Arg 1:", model_file)
+    print("Arg 2:", outputName)
+    print("Path:", outPath)
+    print("BaseName:", outName)
+    print(model_soup.prettify()[0:186])
+    print("Nombre del modelo:", model_name)
+
+    model_attributes = xmile_parser(model_soup)
+
+    # output model file
+    with open(outPath + "/" + outputName + ".R", "w") as foutput:
+        model_output = XmileModel(model_name,
+                                  model_attributes["stocks"],
+                                  model_attributes["auxs"]).show()
+        foutput.writelines(model_output)
 
 
 # %%Model classes
@@ -49,11 +80,12 @@ class XmileStock:
         self.eqn = self.eqn.strip(" ")
 
     def show(self):
-        print("STOCK")
-        print("Name:", self.name)
-        print("Units:", self.units)
-        print("Initial value:", self.init)
-        print("Equation:", self.eqn)
+        stock_report = "".join(["STOCK",
+                                "\n  Name:", self.name,
+                                "\n  Units:", self.units,
+                                "\n  Initial value:", str(self.init),
+                                "\n  Equation:", self.eqn, "\n"])
+        return stock_report
 
 
 class XmileAux:
@@ -75,15 +107,20 @@ class XmileAux:
             self.type = "value"
 
     def show(self):
-        print("CONVERTER")
-        print("Name:", self.name)
-        print("Type:", self.type)
+        aux_report = "".join(["CONVERTER",
+                              "\n  Name:", self.name,
+                              "\n  Type:", self.type])
         if self.type is "value":
-            print("Value:", self.value)
-            print("Units:", self.units)
+            aux_report = "".join([aux_report,
+                                  "\n  Value:", self.value,
+                                  "\n  Units:", self.units])
         else:
-            print("Equation: GRAPH(%s)" % self.eqn)
-            print("Data series:", ", ".join([str(x) for x in self.series]))
+            aux_report = "".join([aux_report,
+                                  "\n  Equation: GRAPH({0})".format(self.eqn),
+                                  "\n  Data series:",
+                                  ", ".join([str(x) for x in self.series])])
+
+        return aux_report
 
 
 class XmileModel:
@@ -94,30 +131,31 @@ class XmileModel:
 
     def rTranslation(self):
         pass
+
         def FunctionTranslator(x):
             return {
                 'EXP': x.lower(),
-                'MIN':x.lower(),
-                'MAX':x.lower(),
-                'MEAN':x.lower(),
-                'SUM':x.lower(),
-                'ABS':x.lower(),
-                'SIN':x.lower(),
-                'COS':x.lower(),
-                'TAN':x.lower(),
-                'LOG10':x.lower(),
-                'SQRT':x.lower(),
-                'ROUND':x.lower(),
-                'LOGN':'log',
-                'ARCTAN':'atan',
-                'TIME':'t',
-                'PI':x.lower(),
-                'INT':'floor',
-                'DT':'DT'
+                'MIN': x.lower(),
+                'MAX': x.lower(),
+                'MEAN': x.lower(),
+                'SUM': x.lower(),
+                'ABS': x.lower(),
+                'SIN': x.lower(),
+                'COS': x.lower(),
+                'TAN': x.lower(),
+                'LOG10': x.lower(),
+                'SQRT': x.lower(),
+                'ROUND': x.lower(),
+                'LOGN': 'log',
+                'ARCTAN': 'atan',
+                'TIME': 't',
+                'PI': x.lower(),
+                'INT': 'floor',
+                'DT': 'DT'
                 }[x]
 
     def show(self):
-        print("Top-Level Model <%s>:" % self.name)
+        items = []
         for stk in self.stocks:
             eqn = stk.name + "(t)" + " = " + stk.name + " (t - dt)"
             fls = ""
@@ -125,26 +163,34 @@ class XmileModel:
                 fls = stk.inflow_name
             if hasattr(stk, "outflow"):
                 fls = fls + " - " + stk.outflow_name
-            items = [eqn + " + (" + fls + ") * dt"]
-            items.extend(["    INIT " + stk.name + " = " + str(stk.init)])
+            item = [eqn + " + (" + fls + ") * dt"]
+            item.extend(["    INIT " + stk.name + " = " + str(stk.init)])
 
             # Define flows
             if hasattr(stk, "inflow"):
-                items.extend(["    INFLOWS:"])
-                items.extend(["        " + stk.inflow_name +
-                              " = " + stk.inflow])
+                item.extend(["    INFLOWS:"])
+                item.extend(["        " + stk.inflow_name +
+                             " = " + stk.inflow])
             if hasattr(stk, "outflow"):
-                items.extend(["    OUTFLOWS:"])
-                items.extend(["        " + stk.outflow_name +
-                              " = " + stk.outflow])
-            print("\n".join(items))
+                item.extend(["    OUTFLOWS:"])
+                item.extend(["        " + stk.outflow_name +
+                             " = " + stk.outflow])
+            items.append("\n".join(item))
+
+        model_report = "".join(["Top-Level Model <{0}>:\n".format(self.name),
+                                "\n".join(items)])
 
         for ax in self.auxs:
             if ax.type is "value":
-                print("%s = %s" % (ax.name, ax.value))
+                model_report = "".join([model_report,
+                                        "\n{0} = {1}".format(ax.name,
+                                                             ax.value)])
             else:
-                print("%s = GRAPH(%s)" % (ax.name, ax.eqn))
-                print(", ".join([str(x) for x in ax.series]))
+                model_report = "".join([model_report,
+                                        "\n{0} = GRAPH({1})".format(ax.name,
+                                                                    ax.eqn),
+                                        ", ".join([str(x)
+                                                   for x in ax.series])])
 
         # Metadata
         flows = set([])
@@ -171,16 +217,17 @@ class XmileModel:
                 "), Flows: ", str(len(flows)), " (",
                 str(len(flows)), "), Converters: ",
                 str(len(self.auxs)), " (", str(len(self.auxs)), ")\n",
-                  "  Constants: ", str(constants), " (", str(constants), "),",
-                  " Equations: ", str(len(self.stocks)), " (",
-                               str(len(self.stocks)), "),",
-                  " Graphicals: ", str(graphs), " (", str(graphs), ")}\n"])
+                "  Constants: ", str(constants), " (", str(constants), "),",
+                " Equations: ", str(len(self.stocks)), " (",
+                str(len(self.stocks)), "),",
+                " Graphicals: ", str(graphs), " (", str(graphs), ")}\n"])
 
-        print(meta_data)
+        model_report = "".join([model_report, meta_data])
+        return model_report
 
 
 # %% Parsers
-def xmile_parser(xmile_file):
+def xmile_parser(xmile_soup):
     # Grammar deffinitions for stocks, flows and aux
     stock_grammar = r"""
         entry =  line+
@@ -279,7 +326,6 @@ def xmile_parser(xmile_file):
         def generic_visit(self, n, vc):
             pass
 
-
     class FlowParser(NodeVisitor):
         def __init__(self, grammar, text):
             self.entry = {}
@@ -328,14 +374,14 @@ def xmile_parser(xmile_file):
     # Extract equations block located in model/variables section of xmile file
     # First get flow equations
     flows = {}
-    for flw in soup.variables.find_all("flow"):
+    for flw in xmile_soup.variables.find_all("flow"):
         flw_parse = FlowParser(flow_grammar, flw.prettify()).entry
         name = flw_parse["flow_name"].replace(" ", "_")
         flows[name] = flw_parse
 
     # Extract stock collection attributes
     stocks_cls = []
-    for stk in soup.variables.find_all("stock"):
+    for stk in xmile_soup.variables.find_all("stock"):
         stock_parse = StockParser(stock_grammar, stk.prettify()).entry
         if stk.inflow:
             stock_parse["inflow"] = flows[stock_parse["inflow"]]
@@ -345,7 +391,7 @@ def xmile_parser(xmile_file):
 
     # Get aux variables
     aux_dict = []
-    for ax in soup.variables.find_all("aux"):
+    for ax in xmile_soup.variables.find_all("aux"):
         aux_parse = AuxParser(aux_grammar, ax.prettify()).entry
         aux_dict.append(XmileAux(aux_parse))
 
@@ -355,17 +401,3 @@ def xmile_parser(xmile_file):
 # %% Read xmile model (like stella 10 onwards)
 if __name__ == '__main__':
     main()
-    with open("SIR_HK (stella 10).stmx") as fp:
-        soup = bs(fp, "lxml")
-    model_name = soup.header.find("name").text
-
-attributs = xmile_parser (soup)
-attributs["auxs"][2].show()
-XmileModel(model_name, attributs["stocks"], attributs["auxs"]).show()
-
-
-stk1 = attributs["stocks"][0]
-stk1.show()
-
-ax1 = attributs["auxs"][2]
-ax1.show()
