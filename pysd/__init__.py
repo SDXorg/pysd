ff7755ed4bb935572b23afb2f7f258bdbffdd67b
James Houghton <James.p.houghton@gmail.com>
2017-09-09 08:03:16 -0400

    Added some integration tests
    Lots of stuff to update to py3, looks like

--- pysd/__init__.py
+++ pysd/__init__.py
@@ -1,7 +1,7 @@
-from pysd.pysd import read_vensim, load
-from pysd.py_backend import functions
-from pysd.py_backend import utils
+from .pysd import read_vensim, read_xmile, load
+from .py_backend import functions
+from .py_backend import utils
 from ._version import __version__
 from . import testing
-from pysd.py_backend.vensim.table2py import read_tabular
+from .py_backend.vensim.table2py import read_tabular
 
--- pysd/py_backend/xmile/SMILE2Py.py
+++ pysd/py_backend/xmile/SMILE2Py.py
@@ -20,58 +20,58 @@ import parsimonious
 from parsimonious.nodes import NodeVisitor
 
 # Here we define which python function each XMILE keyword corresponds to
-dictionary = {"abs":"abs", "int":"int", "exp":"np.exp", "inf":"np.inf", "log10":"np.log10",
-              "pi":"np.pi", "sin":"np.sin", "cos":"np.cos", "sqrt":"np.sqrt", "tan":"np.tan",
-              "lognormal":"np.random.lognormal", "normal":"np.random.normal", 
-              "poisson":"np.random.poisson", "ln":"np.ln", "exprnd":"np.random.exponential",
-              "random":"np.random.rand", "min":"min", "max":"max", "arccos":"np.arccos",
-              "arcsin":"np.arcsin", "arctan":"np.arctan",
-              "if_then_else":"functions.if_then_else",
-              "step":"functions.step", "pulse":"functions.pulse"
-             }
+dictionary = {"abs": "abs", "int": "int", "exp": "np.exp", "inf": "np.inf", "log10": "np.log10",
+              "pi": "np.pi", "sin": "np.sin", "cos": "np.cos", "sqrt": "np.sqrt", "tan": "np.tan",
+              "lognormal": "np.random.lognormal", "normal": "np.random.normal",
+              "poisson": "np.random.poisson", "ln": "np.ln", "exprnd": "np.random.exponential",
+              "random": "np.random.rand", "min": "min", "max": "max", "arccos": "np.arccos",
+              "arcsin": "np.arcsin", "arctan": "np.arctan",
+              "if_then_else": "functions.if_then_else",
+              "step": "functions.step", "pulse": "functions.pulse"
+              }
 
 operators = {
-              "and": "and", "or": "or", "not": "not",
-              "=":"==", "<=":"<=", "<":"<", ">=":">=", ">":">", 
-              "^":"**", "+": "+", "-": "-", 
-              "*": "*", "/": "/", "mod": "%"       
-            }
+    "and": "and", "or": "or", "not": "not",
+    "=": "==", "<=": "<=", "<": "<", ">=": ">=", ">": ">",
+    "^": "**", "+": "+", "-": "-",
+    "*": "*", "/": "/", "mod": "%"
+}
 
-keywords = '|'.join(['%s'%key for key in reversed(sorted(dictionary.keys(), key=len))])
+keywords = '|'.join(['%s' % key for key in reversed(sorted(dictionary.keys(), key=len))])
 
 grammar = (
-    'PrimaryLogic = InvertedLogic / Logic                                                       \n'+
-    'InvertedLogic = ~"not"i _ Logic                                                            \n'+
-    'Logic       = Condition _ Logical*                                                         \n'+
-    'Logical     = ~"and|or"i _ Condition                                                   \n'+
+    'PrimaryLogic = InvertedLogic / Logic                                                       \n' +
+    'InvertedLogic = ~"not"i _ Logic                                                            \n' +
+    'Logic       = Condition _ Logical*                                                         \n' +
+    'Logical     = ~"and|or"i _ Condition                                                   \n' +
 
-    'Condition   = Term _ Conditional*                                                          \n'+
-    'Conditional = ("<=" / "<" / ">=" / ">" / "=") _ Term                                       \n'+
+    'Condition   = Term _ Conditional*                                                          \n' +
+    'Conditional = ("<=" / "<" / ">=" / ">" / "=") _ Term                                       \n' +
 
-    'Term        = Factor _ Additive*                                                           \n'+
-    'Additive    = ("+"/"-") _ Factor                                                           \n'+
+    'Term        = Factor _ Additive*                                                           \n' +
+    'Additive    = ("+"/"-") _ Factor                                                           \n' +
 
-    'Factor      = ExpBase _ Multiplicative*                                                    \n'+
-    'Multiplicative = ("*" / "/" / ~"mod"i) _ ExpBase                                           \n'+
+    'Factor      = ExpBase _ Multiplicative*                                                    \n' +
+    'Multiplicative = ("*" / "/" / ~"mod"i) _ ExpBase                                           \n' +
 
-    'ExpBase  = Primary _ Exponentive*                                                          \n'+
-    'Exponentive = "^" _ Primary                                                                \n'+
+    'ExpBase  = Primary _ Exponentive*                                                          \n' +
+    'Exponentive = "^" _ Primary                                                                \n' +
 
-    'Primary  = Call / Parens / Signed / Number / Reference                                     \n'+
-    'Parens   = "(" _ PrimaryLogic _ ")"                                                        \n'+
-    'Call     = Keyword _ "(" _ ArgList _ ")"                                                   \n'+
-    'ArgList  = AddArg+                                                                         \n'+
-    'AddArg   = ","* _ PrimaryLogic                                                             \n'+
-    'Signed   = ("-"/"+") Primary                                                               \n'+
-    'Number   = ((~"[0-9]"+ "."? ~"[0-9]"*) / ("." ~"[0-9]"+)) (("e"/"E") ("-"/"+") ~"[0-9]"+)? \n'+
-    'Reference = Identifier _                                                                   \n'+
-    'Identifier = ~"[a-zA-Z]" ~"[a-zA-Z0-9_\$]"*                                              \n'+
+    'Primary  = Call / Parens / Signed / Number / Reference                                     \n' +
+    'Parens   = "(" _ PrimaryLogic _ ")"                                                        \n' +
+    'Call     = Keyword _ "(" _ ArgList _ ")"                                                   \n' +
+    'ArgList  = AddArg+                                                                         \n' +
+    'AddArg   = ","* _ PrimaryLogic                                                             \n' +
+    'Signed   = ("-"/"+") Primary                                                               \n' +
+    'Number   = ((~"[0-9]"+ "."? ~"[0-9]"*) / ("." ~"[0-9]"+)) (("e"/"E") ("-"/"+") ~"[0-9]"+)? \n' +
+    'Reference = Identifier _                                                                   \n' +
+    'Identifier = ~"[a-zA-Z]" ~"[a-zA-Z0-9_\$]"*                                              \n' +
 
-    '_ = spacechar*                                                                             \n'+
-    'spacechar = " "* ~"\t"*                                                                    \n'+
+    '_ = spacechar*                                                                             \n' +
+    'spacechar = " "* ~"\t"*                                                                    \n' +
 
-    'Keyword = ~"%s"i  \n'%keywords
-           )
+    'Keyword = ~"%s"i  \n' % keywords
+)
 
 
 class SMILEParser(NodeVisitor):
@@ -90,14 +90,14 @@ class SMILEParser(NodeVisitor):
 
     def visit_Keyword(self, n, vc):
         return dictionary[n.text.lower()]
-    
+
     def visit_Reference(self, n, vc):
         convertedIdentifier = vc[0]
         if convertedIdentifier == 'dt':
             convertedIdentifier = 'time_step'
-        
+
         if self.context == 'eqn':
-            return 'self.'+convertedIdentifier+'()'
+            return 'self.' + convertedIdentifier + '()'
         elif self.context == 'defn':
             return convertedIdentifier
 
@@ -110,7 +110,7 @@ class SMILEParser(NodeVisitor):
 
     def visit_Call(self, n, vc):
         Translated_Keyword, _1, lparen, _2, args, _3, rparen = vc
-        return Translated_Keyword+'('+', '.join(args)+')'
+        return Translated_Keyword + '(' + ', '.join(args) + ')'
 
     def visit_ArgList(self, n, args):
         return args
@@ -128,7 +128,7 @@ class SMILEParser(NodeVisitor):
     visit_Logical = operationVisitor
     visit_Additive = operationVisitor
     visit_InvertedLogic = operationVisitor
-    
+
     def generic_visit(self, n, vc):
         """
         Replace childbearing nodes with a list of their children;
@@ -136,5 +136,3 @@ class SMILEParser(NodeVisitor):
         for empty nodes, return an empty string.
         """
         return ''.join(filter(None, vc)) or n.text or ''
-
-
