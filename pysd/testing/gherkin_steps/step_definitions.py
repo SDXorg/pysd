b39c377e4739cc0a724e8f172f6e619a64f9971b
James Houghton <James.p.houghton@gmail.com>
2017-09-26 17:20:15 -0400

    Basic Gherkin Working
    Just a few possible tests, but a nice POC.

--- pysd/testing/gherkin_steps/step_definitions.py
+++ pysd/testing/gherkin_steps/step_definitions.py
@@ -1,11 +1,15 @@
 from behave import *
 import pysd
 import nose.tools as test
+import os
 
 
 @given('The model "{model_file}"')
 def load_model(context, model_file):
     ext = model_file.split('.')[-1].lower()
+    dir = context._config.base_dir
+    model_file = os.path.join(dir, model_file)
+
     if ext == 'mdl':
         context.model = pysd.read_vensim(model_file)
     elif ext == 'xmile':
@@ -16,7 +20,7 @@ def load_model(context, model_file):
         raise ValueError('Unknown model extension')
 
 
-@when('"{variable}" is set to "{value}"')
+@when('{variable} is set to {value:g}')
 def set_value(context, variable, value):
     context.model.set_components({variable: value})
 
@@ -26,7 +30,13 @@ def run_model(context):
     context.result = context.model.run()
 
 
-@then('"{variable}" are immediately "{value}"')
+@then('{variable} is immediately {value:g}')
 def check_immediate(context, variable, value):
-    test.assert_equal(context.model.components[variable],
+    test.assert_equal(getattr(context.model.components,
+                              context.model.components._namespace[variable])(),
                       value)
+
+
+@then('{variable} becomes {value:g} at time {time:g}')
+def check_future(context, variable, value, time):
+    test.assert_equal(context.result[variable].loc[time], value)
