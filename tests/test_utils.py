f336343e7f63806344953822acd749108c21e312
Julien Malard <julien.malard@mail.mcgill.ca>
2018-04-30 10:40:52 -0400

    Trying without Unicode encoding on canonical output file reading.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,10 +22,10 @@ def runner(model_file):
 
     # load canonical output
     try:
-        canon = pd.read_csv(directory + '/output.csv', index_col='Time', encoding='UTF-8')
+        canon = pd.read_csv(directory + '/output.csv', index_col='Time')
     except IOError:
         try:
-            canon = pd.read_table(directory + '/output.tab', index_col='Time', encoding='UTF-8')
+            canon = pd.read_table(directory + '/output.tab', index_col='Time')
         except IOError:
             raise IOError('Canonical output file not found')
 
