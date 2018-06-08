441f5ebbb38d5ec82a576061506a2c9e6840a593
Alexey Prey Mulyukin <alexprey@yandex.ru>
2018-06-09 01:42:34 +0300

    Change test_lookup.xmile model as independ graphical node of model.

--- tests/integration_test_xmile_pathway.py
+++ tests/integration_test_xmile_pathway.py
@@ -402,6 +402,7 @@ class TestIntegrationExamples(unittest.TestCase):
         output, canon = runner('test-models/tests/trend/test_trend.xmile')
         assert_frames_close(output, canon, rtol=rtol)
 
+    @unittest.skip('no xmile')
     def test_xidz_zidz(self):
         from .test_utils import runner, assert_frames_close
         output, canon = runner('test-models/tests/xidz_zidz/xidz_zidz.xmile')
