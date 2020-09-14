9c450ef6076caf12d1b2a633ee10b653444e0578
Diego Rodrigo <diego-break-@hotmail.com>
2020-09-14 21:47:05 +0200

    removed debug prints

--- tests/unit_test_pysd.py
+++ tests/unit_test_pysd.py
@@ -323,7 +323,6 @@ class TestPySD(unittest.TestCase):
     def test_set_state(self):
         import pysd
         model = pysd.read_vensim(test_model)
-        print("a")
         initial_temp = model.components.teacup_temperature()
         model.components.teacup_temperature()
         model.components.teacup_temperature()
@@ -331,15 +330,12 @@ class TestPySD(unittest.TestCase):
         model.components.teacup_temperature()
         model.components.teacup_temperature()
         model.components.teacup_temperature()
-        print("b")
         initial_time = model.components.time()
 
         new_time = np.random.rand()
 
         # Test that we can set with real names
         model.set_state(new_time, {'Teacup Temperature': 500})
-        print("c")
-        print("comprobando", initial_temp, model.components.teacup_temperature())
         self.assertNotEqual(initial_temp, 500)
         self.assertEqual(model.components.teacup_temperature(), 500)
 
