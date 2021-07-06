1469a910fb2f24b127945747487c6188d6149a4f
Eneko Martin Martinez <eneko.martin.martinez@gmail.com>
2021-07-06 09:52:38 +0200

    Improve benchmarking tools

--- pysd/tools/benchmarking.py
+++ pysd/tools/benchmarking.py
@@ -48,7 +48,9 @@ def runner(model_file, canonical_file=None, transpose=False):
         else:
             raise FileNotFoundError('\nCanonical output file not found.')
 
-    canon = load_outputs(canonical_file, transpose)
+    canon = load_outputs(canonical_file,
+                         transpose=transpose,
+                         encoding=detect_encoding(canonical_file))
 
     # load model
     if model_file.lower().endswith('.mdl'):
@@ -63,7 +65,7 @@ def runner(model_file, canonical_file=None, transpose=False):
     return model.run(return_columns=canon.columns), canon
 
 
-def load_outputs(file_name, transpose=False):
+def load_outputs(file_name, transpose=False, columns=None, encoding=None):
     """
     Load outputs file
 
@@ -76,6 +78,16 @@ def load_outputs(file_name, transpose=False):
         If True reads transposed outputs file, i.e. one variable per row.
         Default is False.
 
+    columns: list or None (optional)
+        List of the column names to load. If None loads all the columns.
+        Default is None. Note, if transpose=False, the loading will be
+        faster as only selected columns will be loaded. If transpose=True
+        the whole file must be read and it will be subselected later.
+
+    encoding: str or None (optional)
+        Encoding type to read output file. Needed if the file has special
+        characters. Default is None.
+
     Returns
     -------
     pandas.DataFrame
@@ -84,19 +96,28 @@ def load_outputs(file_name, transpose=False):
     """
     read_func = {'.csv': pd.read_csv, '.tab': pd.read_table}
 
+    if columns:
+        columns = set(columns)
+        if not transpose:
+            columns.add("Time")
+
     for end, func in read_func.items():
         if file_name.lower().endswith(end):
             if transpose:
                 out = func(file_name,
-                           encoding=_detect_encoding(file_name),
+                           encoding=encoding,
                            index_col=0).T
+                if columns:
+                    out = out[columns]
             else:
                 out = func(file_name,
-                           encoding=_detect_encoding(file_name),
-                           index_col='Time')
+                           encoding=encoding,
+                           usecols=columns,
+                           index_col="Time")
 
             out.index = out.index.astype(float)
-            return out
+            # return the dataframe removing nan index values
+            return out[~np.isnan(out.index)]
 
     raise ValueError(
         f"\nNot able to read '{file_name}'. "
@@ -178,18 +199,44 @@ def assert_frames_close(actual, expected, assertion="raise",
         http://nbviewer.jupyter.org/gist/jiffyclub/ac2e7506428d5e1d587b
 
     """
-    assert (isinstance(actual, pd.DataFrame) and
-            isinstance(expected, pd.DataFrame)), \
-        'Inputs must both be pandas DataFrames.'
+    if not isinstance(actual, pd.DataFrame)\
+       or not isinstance(expected, pd.DataFrame):
+        raise TypeError('\nInputs must both be pandas DataFrames.')
 
-    assert set(expected.columns) == set(actual.columns), \
-        'test set columns must be equal to those in actual/observed set.'
+    expected_cols, actual_cols = set(expected.columns), set(actual.columns)
+
+    if expected_cols != actual_cols:
+        # columns are not equal
+        message = ""
+
+        if actual_cols.difference(expected_cols):
+            columns = ["'" + col + "'" for col
+                       in actual_cols.difference(expected_cols)]
+            columns = ", ".join(columns)
+            message += '\nColumns ' + columns\
+                       + ' from actual values not found in expected values.'
+
+        if expected_cols.difference(actual_cols):
+            columns = ["'" + col + "'" for col
+                       in expected_cols.difference(actual_cols)]
+            columns = ", ".join(columns)
+            message += '\nColumns ' + columns\
+                       + ' from expected values not found in actual values.'
+
+        if assertion == "raise":
+            raise ValueError(
+                '\nColumns from actual and expected values must be equal.'
+                + message)
+        else:
+            warnings.warn(message)
+
+    columns = actual_cols.intersection(expected_cols)
 
     assert np.all(np.equal(expected.index.values, actual.index.values)), \
         'test set and actual set must share a common index' \
         'instead found' + expected.index.values + 'vs' + actual.index.values
 
-    for col in expected.columns:
+    for col in columns:
         # if for Vensim outputs where constant values are only in the first row
         if np.isnan(expected[col].values[1:]).all():
             expected[col] = expected[col].values[0]
@@ -245,7 +292,7 @@ def assert_allclose(x, y, rtol=1.e-5, atol=1.e-5):
     assert np.all(np.less_equal(abs(x - y), atol + rtol * abs(y)))
 
 
-def _detect_encoding(filename):
+def detect_encoding(filename):
     """
     Detects the encoding of a file.
 
