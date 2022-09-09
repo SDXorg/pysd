bae17f74dc270f1ba5d76098e9dceb7e3d07b688
Roger Samsó <rogersamso@gmail.com>
2022-09-09 12:39:29 +0200

    update whats new

--- pysd/cli/parser.py
+++ pysd/cli/parser.py
@@ -29,9 +29,7 @@ def check_output(string):
     Checks that out put file ends with .tab or .csv
 
     """
-    if (not string.endswith('.tab') and
-            not string.endswith('.csv') and
-                not string.endswith('.nc')):
+    if not string.endswith(('.tab', '.csv', '.nc')):
         parser.error(
             f'when parsing {string}'
             '\nThe output file name must be .tab, .csv or .nc...')
